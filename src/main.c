0689c5c4aee5ec19cd6fa69a790555e76e020b07
RmbRT <steffen@sm2.network>
2023-03-29 15:24:09 +0200

    Fix linking, simple main as test.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
+#include <ncurses/ncurses.h>
+
 
 int main(int argc, char** argv)
 {
 	(void) argc;
 	(void) argv;
+
+	initscr();
+	cbreak();
+	noecho();
+
+	getch();
+
+	endwin();
+
 	return 0;
 }
